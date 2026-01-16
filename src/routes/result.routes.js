756a03eaa4e593e9601b83829117312d5b6c4217
SUBHASIS <mukherjeebasis@gmail.com>
2026-01-16 09:37:00 +0530

    update

--- src/routes/result.routes.js
+++ src/routes/result.routes.js
@@ -6,7 +6,6 @@ const resultCtrl = require("../controllers/user.controller");
 // email pdf again (optional)
 router.post("/result/email", resultCtrl.emailResultPDF);
 
-// get user results
-// router.get("/results/:userId", resultCtrl.getUserResults);
+
 
 module.exports = router;
